132388dafd492df602b627b63abc63dd9be33129
interjc <i@interjc.net>
2013-07-05 16:25:42 +0800

    select.text.js

---
+++ JS/select.text.js
@@ -0,0 +1,16 @@
+function selectText(element) {
+    var doc = document;
+    var text = doc.getElementById(element);    
+
+    if (doc.body.createTextRange) { // ms
+        var range = doc.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { // moz, opera, webkit
+        var selection = window.getSelection();            
+        var range = doc.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
